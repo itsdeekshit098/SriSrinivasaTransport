7c559a2f91bc636fad53612a8ff3e64a134ad4c0
DEEKSHITH REDDY <itsdeekshit098@gmail.com>
2025-08-04 12:08:27 +0530

    feat: added responsiveness adn changed the navbar

--- srisrinivasatransport/src/pages/_app.tsx
+++ srisrinivasatransport/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../app/store";
-import Navbar from "@/components/navbar";
+import Navbar from "../components/navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

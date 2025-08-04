644f86d7a2d38b77f2e71dea700109ce960e6f6a
DEEKSHITH REDDY <itsdeekshit098@gmail.com>
2025-08-04 12:19:39 +0530

    added redux tookit to the application for managing and storing the data efficiently

--- srisrinivasatransport/src/pages/_app.tsx
+++ srisrinivasatransport/src/pages/_app.tsx
@@ -4,7 +4,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../app/store";
-import Navbar from "../components/navbar";
+import Navbar from "@/components/Navbar";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
